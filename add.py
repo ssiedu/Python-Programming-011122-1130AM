ecfbce921c9c734fe6a95cb2e098e0f395be3cc0
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-08 12:30:20 +0530

    program

---
+++ add.py
@@ -0,0 +1,3 @@
+num1= eval(input("Enter First number :"))
+num2= eval(input("Enter Second Number : "))
+print("Sum of two numbers : %.2f"%(num1+num2))
---
+++ print.py
@@ -0,0 +1,4 @@
+print(10,20,30,sep="+",end="=")
+print(10+20+30)
+print(10,20,sep="+",end="*")
+print(30)
