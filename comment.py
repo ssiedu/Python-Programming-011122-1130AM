3a875cabaf02f4620f139cadab6b7516e174d059
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-15 19:52:16 +0530

    comment

---
+++ comment.py
@@ -0,0 +1,17 @@
+# this is a single line comment
+print("Single line comment")# single line
+'''This is
+a multiline
+comment'''
+print("Multiline comment")
+#this is add function 
+def add():
+      
+      """This is a function to
+add two values"""
+      print(10+20)
+      
+
+#print(add())
+add()
+print(add.__doc__)
