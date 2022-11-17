edfbb4341a3ea5139fbdb283ebfcbb47aea6c1d6
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-17 20:01:34 +0530

    program

---
+++ relational.py
@@ -0,0 +1,8 @@
+a=eval(input("Enter First Number : "))
+b=eval(input("Enter Second Number :"))
+print("Greater than : ", a>b)
+print(" Less Than   : ", a<b)
+print("Greater than or equal too : ", a>=b)
+print("Less than or equal too : ", a<=b)
+print("Equal too : ",a==b)
+print("Not Equal to : ",a!=b)
---
+++ swap1.py
@@ -0,0 +1,16 @@
+num1=eval(input("Enter First Number : "))
+num2=eval(input("Enter Second Number : "))
+print("Before Swapping ")
+print("First number : ",num1)
+print("Second number : ",num2)
+'''temp=num1;
+num1=num2
+num2=temp'''
+'''num1=num1+num2#num1 = 10+20 =30
+num2=num1-num2#num2= 30-20 = 10
+num1=num1-num2#num1=30-10 = 20'''
+
+num1,num2 = num2,num1
+print("After swapping")
+print("First number : ",num1)
+print("Second number : ",num2)
