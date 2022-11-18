4191eed4ea193c9ac026804b663c9cffd4077afb
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-11-18 15:08:51 +0530

    swap

---
+++ swap2.py
@@ -0,0 +1,17 @@
+a=int(input("Enter First Number : "))
+b=int(input("Enter Second Number : "))
+print("Before Swapping ")
+print("Value of a : ",a)
+print("value of b : ",b)
+
+'''a=a*b;
+b=a/b
+a=a/b'''
+
+a=a^b
+b=a^b
+a=a^b
+
+print("After swap")
+print("value of a : ",a)
+print("value of b : ",b)
